635a807cc34a95cae923d7413cffb665ac3ef545
zoniha <awaawapurin@gmail.com>
2020-12-27 21:48:16 +0900

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'hello', to: 'hello#index'
+  get 'hello/index'
+  post 'hello', to: 'hello#index'
+  post 'hello/index'
 end
