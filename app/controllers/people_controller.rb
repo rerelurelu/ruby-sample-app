be2a4c42e116f80af05dd22876d88dcfe995a996
zoniha <awaawapurin@gmail.com>
2020-12-31 18:03:21 +0900

    updated

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -15,10 +15,20 @@ class PeopleController < ApplicationController
   end
 
   def create
-    if request.post?
-      Person.create(person_params)
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to '/people'
+    else
+      re = ''
+      @person.errors.messages.each do |key, vals|
+        vals.each do |val|
+          re += '<span style="color:red">' + key.to_s +
+            '</span> ' + val + '<br>'
+        end
+      end
+      @msg = re.html_safe
+      render 'add'
     end
-    redirect_to '/people'
   end
 
   def edit
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ApplicationRecord
+  validates :name, presence: true, format: {with: /\A[a-zA-Z]+\z/}
+  validates :age, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
