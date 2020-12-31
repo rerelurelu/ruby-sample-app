1ae3e5825c33daa7b462e7474c0a2313aa981f8f
zoniha <awaawapurin@gmail.com>
2020-12-31 19:16:23 +0900

    updated

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
   validates :name, presence: true, format: {with: /\A[a-zA-Z]+\z/}
   validates :age, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :mail, email: true
 end
